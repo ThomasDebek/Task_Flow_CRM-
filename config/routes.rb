b576f8aafb50958388b776f966d90d7a158c57c2
ThomasDebek <tom.debek@gmail.com>
2026-04-14 20:39:24 +0200

    routes: set convert

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   resources :clients
 
+
+  resources :leads do
+    member do
+      post :convert
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
