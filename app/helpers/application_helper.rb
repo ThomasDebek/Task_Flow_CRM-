9b85a62235a0e2b786fc247c4be589ca64952559
ThomasDebek <tom.debek@gmail.com>
2026-04-14 20:46:26 +0200

    status_badge: update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,8 @@ module ApplicationHelper
       "#{base} bg-slate-100 text-slate-700"
     when "vip"
       "#{base} bg-purple-100 text-purple-800"
+    when "new"
+      "#{base} bg-yellow-100 text-yellow-800"
     else
       "#{base} bg-gray-100 text-gray-700"
     end
