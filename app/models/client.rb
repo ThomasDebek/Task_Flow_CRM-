3b49946fb2f5133386408872e0330b82365b01c2
ThomasDebek <tom.debek@gmail.com>
2026-05-28 22:49:33 +0200

    models: conection note with client

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,6 +9,7 @@ class Client < ApplicationRecord
   validates :status, presence: true
 
   has_many :tasks, dependent: :destroy
+  has_many :notes, dependent: :destroy
 
   pg_search_scope :search,
                   against: [:name, :email, :phone, :company_name],
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :client
+  validates :content, presence: true
 end
