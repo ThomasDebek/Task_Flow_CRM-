473cc3553664fe3e21189bdae3be07bd3ee60fc5
ThomasDebek <tom.debek@gmail.com>
2026-05-28 22:56:14 +0200

    controller_notes: create new method create destroy]

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,32 @@
+class NotesController < ApplicationController
+
+  before_action :set_client
+
+  def create
+    @note = @client.notes.create(note_params)
+    if @note.save
+      redirect_to @client, notice: "Note created."
+    else
+      redirect_to @client, alert: "Failed to create note."
+    end
+  end
+
+  def destroy
+    @note = @client.notes.find(params[:id])
+    if @note.destroy
+      redirect_to @client, notice: "Note deleted."
+    else
+      redirect_to @client, alert: "Failed to delete note."
+    end
+  end
+
+
+  private
+  def set_client
+    @client = Client.find(params[:client_id])
+  end
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
+end
