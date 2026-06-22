37d6464a19324e3989be7260127495392ff9f5ad
ThomasDebek <tom.debek@gmail.com>
2026-06-22 22:39:55 +0200

    crates a new lead test

--- spec/requests/leads_spec.rb
+++ spec/requests/leads_spec.rb
@@ -14,4 +14,26 @@ RSpec.describe "Leads", type: :request do
       expect(response).to redirect_to(new_user_session_path)
     end
   end
+
+  describe "POST /create" do
+    it "creates a new lead" do
+      user = create(:user)
+      sign_in user
+      expect do
+        post leads_path, params: {
+          lead: {
+            first_name: "John",
+            last_name: "Doe",
+            email: "john@example.com",
+            phone: "123456789",
+            company_name: "Test Company",
+            status: "new"
+          }
+        }
+      end.to change(Lead, :count).by(1)
+    end
+  end
+
+
+  
 end
