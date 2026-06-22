08026745a6fd5f6d0744ce7ed3b4c7361cea9943
ThomasDebek <tom.debek@gmail.com>
2026-06-22 22:37:17 +0200

    return a successful respone and redirect when pagi if not logged

---
+++ spec/requests/leads_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Leads", type: :request do
+  describe "GET /index" do
+    it "returns a successful response" do
+      user = create(:user)
+      sign_in user
+      get leads_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "redirects to login page if not logged in" do
+      get leads_path
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+end
