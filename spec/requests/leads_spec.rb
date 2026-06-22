b02e3340f294c67b5272887f86358b96c202ee1b
ThomasDebek <tom.debek@gmail.com>
2026-06-22 22:41:39 +0200

    rspec: test assigns the lead

--- spec/requests/leads_spec.rb
+++ spec/requests/leads_spec.rb
@@ -35,5 +35,42 @@ RSpec.describe "Leads", type: :request do
   end
 
 
-  
+  describe "POST /create" do
+    it "creates a new lead" do
+      user = create(:user)
+      sign_in user
+      expect do
+        post leads_path, params: {
+          lead: {
+            first_name: "John",
+            last_name: "Doe",
+            email: "john@example.com",
+            phone: "123456789",
+            company_name: "Test Company",
+            status: "new"
+          }
+        }
+      end.to change(Lead, :count).by(1)
+    end
+
+    it "assigns the lead to the current user" do
+      user = create(:user)
+      sign_in user
+      post leads_path, params: {
+        lead: {
+          first_name: "John",
+          last_name: "Doe",
+          email: "john@example.com",
+          phone: "123456789",
+          company_name: "Test Company",
+          status: "new"
+        }
+      }
+      expect(Lead.last.user).to eq(user)
+    end
+
+  end
+
+
+
 end
