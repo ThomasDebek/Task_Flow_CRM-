a3850fae1d58365e478f496a380c8d67aec83a01
ThomasDebek <tom.debek@gmail.com>
2026-06-30 23:30:24 +0200

    Add first system spec for client creation

--- spec/system/clients/clients_create_clients_spec.rb
+++ spec/system/clients/clients_create_clients_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Clients::CreateClients", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  it "allows a user to create a client" do
+    user = create(:user)
+    sign_in user
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    visit new_client_path
+
+    fill_in "Name", with: "System Test Client"
+    fill_in "Email", with: "system@example.com"
+    fill_in "Phone", with: "123456789"
+    fill_in "Company name", with: "System Company"
+    select "active", from: "Status"
+
+    click_button "Create Client"
+
+    expect(page).to have_content("Client was successfully created")
+    expect(Client.last.name).to eq("System Test Client")
+  end
 end
